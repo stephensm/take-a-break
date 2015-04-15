b6d8fa44c3ecaaed591c894707a3e9e985faca46
Marissa Stephens <marissas@mit.edu>
2015-04-15 14:43:49 -0400

    Made cool links to event pages. Also can autogenerate more ideas

--- tab.js
+++ tab.js
@@ -1,5 +1,35 @@
  var i=1;
+
 $( document ).ready(function() {
+	if(getUrlParameter('event')){
+		showEvent();
+		var event=getUrlParameter('event');
+		console.log(event);
+		if (event==='mario'){
+			makeMario();
+		}
+		if (event==='bubbles'){
+			makeBubbles();
+		}
+		if (event==='icecream'){
+			makeIce();
+		}
+		if (event==='cannolies'){
+			makeCannolies();
+		}
+		if (event==='sushi'){
+			makeSushi();
+		}
+		if (event==='donuts'){
+			makeDonuts()
+		}
+		if (event==='tea'){
+			makeTea();
+		}
+		if (event==='froyo'){
+			makeFroyo();
+		}
+	}
 	$('#datetimepicker1').datepicker({    
 		autoclose: true,
     	todayHighlight: true});
@@ -31,6 +61,105 @@ function addRow(item){
 function suggestIdea(){
 	var title=document.getElementById('break-name').value.toLowerCase();
 	if(title.indexOf("ice cream")>-1){
+		makeIce();
+	}
+}
+function getRandomIdea(){
+	var event=Math.floor(Math.random() * 8) ;
+	if (event===0){
+			makeMario();
+		}if (event===1){
+			makeBubbles();
+		}if (event===2){
+			makeIce();
+		}if (event===3){
+			makeCannolies();
+		}if (event===4){
+			makeSushi();
+		}if (event===5){
+			makeDonuts()
+		}if (event===6){
+			makeTea();
+		}if (event===7){
+			makeFroyo();
+		}
+}
+function showEvent(){
+	$('#make-event').hide();
+	document.getElementById("event").style.display = "inherit";
+	document.getElementById("no-events").remove();
+	
+}
+function makeBubbles(){
+		$("#allergies-list").empty();
+	document.getElementById('break-name').value="Bubble Party";
+		$("#to-do").find("tr:gt(0)").remove();
+		addRow("Soap");
+		addRow("Wire");
+		addRow("Bubble Wands");
+		addRow("Buckets");
+}
+function makeMario(){
+		$("#allergies-list").empty();
+	document.getElementById('break-name').value="Mario Party";
+		$("#to-do").find("tr:gt(0)").remove();
+		addRow("Mario Cart");
+		addRow("Wii");
+}
+function makeCannolies(){
+		$("#allergies-list").empty();
+	document.getElementById('break-name').value="Cannolies";
+		$("#to-do").find("tr:gt(0)").remove();
+		addRow("Plates");
+		addRow("Cannolies");
+		addRow("Milk");
+		var ul = document.getElementById("allergies-list");
+		var li1 = document.createElement("li");
+		li1.className="list-group-item";
+		var text1 = document.createTextNode("Dairy (10) ");
+		var li2 = document.createElement("li");
+		li2.className="list-group-item";
+		var text2 = document.createTextNode("Vegan (3) ");
+		var li3 = document.createElement("li");
+		li3.className="list-group-item";
+		var text3 = document.createTextNode("Nuts (15) ");
+		li1.appendChild(text1);
+		li2.appendChild(text2);
+		li3.appendChild(text3);
+		ul.appendChild(li1);
+		ul.appendChild(li2);
+		ul.appendChild(li3);
+}
+function makeSushi(){
+		$("#allergies-list").empty();
+	document.getElementById('break-name').value="Sushi";
+		$("#to-do").find("tr:gt(0)").remove();
+		addRow("Rice");
+		addRow("Seaweed (Nori) ");
+		addRow("Pickeld Ginger");
+		addRow("wassabi");
+		addRow("cucumbers");
+		addRow("avacados");
+		addRow("salmon");
+		addRow("tuna");
+		var ul = document.getElementById("allergies-list");
+		var li1 = document.createElement("li");
+		li1.className="list-group-item";
+		var text1 = document.createTextNode("Vegan (10) ");
+		var li2 = document.createElement("li");
+		li2.className="list-group-item";
+		var text2 = document.createTextNode("Vegatarian (3) ");
+		var li3 = document.createElement("li");
+		li3.className="list-group-item";
+		var text3 = document.createTextNode("Fish (1) ");
+		li1.appendChild(text1);
+		li2.appendChild(text2);
+		li3.appendChild(text3);
+		ul.appendChild(li1);
+		ul.appendChild(li2);
+		ul.appendChild(li3);
+}
+function makeIce(){
 		$("#allergies-list").empty();
 		$("#to-do").find("tr:gt(0)").remove();
 		addRow("Ice cream");
@@ -42,13 +171,13 @@ function suggestIdea(){
 		var ul = document.getElementById("allergies-list");
 		var li1 = document.createElement("li");
 		li1.className="list-group-item";
-		var text1 = document.createTextNode("Dairy");
+		var text1 = document.createTextNode("Dairy (10) ");
 		var li2 = document.createElement("li");
 		li2.className="list-group-item";
-		var text2 = document.createTextNode("Vegan");
+		var text2 = document.createTextNode("Vegan (3) ");
 		var li3 = document.createElement("li");
 		li3.className="list-group-item";
-		var text3 = document.createTextNode("Nuts");
+		var text3 = document.createTextNode("Nuts (15) ");
 		li1.appendChild(text1);
 		li2.appendChild(text2);
 		li3.appendChild(text3);
@@ -56,22 +185,49 @@ function suggestIdea(){
 		ul.appendChild(li2);
 		ul.appendChild(li3);
 	}
-}
-function getRandomIdea(){
-	$("#allergies-list").empty();
-	document.getElementById('break-name').value="Bubble Party";
+function makeFroyo(){
+		$("#allergies-list").empty();
 		$("#to-do").find("tr:gt(0)").remove();
-		addRow("Soap");
-		addRow("Wire");
-		addRow("Bubble Wands");
-		addRow("Buckets");
-
+		addRow("Froyo");
+		addRow("Cherries");
+		addRow("Bowls");
+		addRow("Sprinkles");
+		addRow("Nuts");
+		addRow("Whipped Cream");
+		var ul = document.getElementById("allergies-list");
+		var li1 = document.createElement("li");
+		li1.className="list-group-item";
+		var text1 = document.createTextNode("Dairy (10) ");
+		var li2 = document.createElement("li");
+		li2.className="list-group-item";
+		var text2 = document.createTextNode("Vegan (3) ");
+		var li3 = document.createElement("li");
+		li3.className="list-group-item";
+		var text3 = document.createTextNode("Nuts (15) ");
+		li1.appendChild(text1);
+		li2.appendChild(text2);
+		li3.appendChild(text3);
+		ul.appendChild(li1);
+		ul.appendChild(li2);
+		ul.appendChild(li3);
+	}
+function makeDonuts(){
+		$("#allergies-list").empty();
+	document.getElementById('break-name').value="Dounuts";
+		$("#to-do").find("tr:gt(0)").remove();
+		addRow("Donuts");
+		addRow("Coffee");
+		addRow("Cups");
+		addRow("Plates");
 }
-function showEvent(){
-	$('#make-event').hide();
-	document.getElementById("event").style.display = "inherit";
-	document.getElementById("no-events").remove();
-	
+function makeTea(){
+		$("#allergies-list").empty();
+	document.getElementById('break-name').value="Bubble Tea";
+		$("#to-do").find("tr:gt(0)").remove();
+		addRow("Ice Tea");
+		addRow("Boba");
+		addRow("straws");
+		addRow("Cups");
 }
 function clearForm(){
 	$("#to-do").find("tr:gt(0)").remove();
@@ -79,3 +235,18 @@ function clearForm(){
 	$("#allergies-list").empty();
 	addRow();
 }
+
+// source http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}   
