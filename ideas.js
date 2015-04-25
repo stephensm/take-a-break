59dcfde7b371b14d98875f8ee8f1d88112147027
Lambert <chul@mit.edu>
2015-04-25 12:25:44 -0400

    major changes

--- ideas.js
+++ ideas.js
@@ -13,7 +13,13 @@ $(document).ready(function(){
       }
     });
 
-    // Voting system - this should be restricted to one vote per user
+    // Tooltips
+    $(".glyphicon-trash").tooltip({
+      content: "Delete"
+    });
+
+    // Voting system - restricted to one vote per user
+    // Upvotes
     $(".glyphicon-plus").click(function(){
       var td = $(this).parent();
       var thisCount = td.find('#count');
@@ -22,7 +28,7 @@ $(document).ready(function(){
       if($(td).hasClass("upvoted")) {
         var number = parseInt(text, 10) - 1;
         thisCount.html(number);
-        $(this).css("background-color", "transparent");
+        $(this).css("background-color", "#BDBDBD");
       }
 
       else if($(td).hasClass("downvoted")) {
@@ -32,7 +38,7 @@ $(document).ready(function(){
         $(td).toggleClass("downvoted");
         $(this).css("background-color", "#009647");
         var downvote = td.find('.glyphicon-minus');
-        $(downvote).css("background-color", "transparent");
+        $(downvote).css("background-color", "#BDBDBD");
       }
 
       else {
@@ -41,9 +47,11 @@ $(document).ready(function(){
         $(this).css("background-color", "#009647");
       }
 
+      $(td).effect("highlight", {}, 500);
       $(td).toggleClass("upvoted");
     });
 
+    // Downvotes
     $(".glyphicon-minus").click(function(){
       var td = $(this).parent();
       var thisCount = td.find('#count');
@@ -52,7 +60,7 @@ $(document).ready(function(){
       if($(td).hasClass("downvoted")) {
         var number = parseInt(text, 10) + 1;
         thisCount.html(number);
-        $(this).css("background-color", "transparent");
+        $(this).css("background-color", "#BDBDBD");
       }
 
       else if($(td).hasClass("upvoted")) {
@@ -62,7 +70,7 @@ $(document).ready(function(){
         $(td).toggleClass("upvoted");
         $(this).css("background-color", "#f43535");
         var upvote = td.find('.glyphicon-plus');
-        $(upvote).css("background-color", "transparent");
+        $(upvote).css("background-color", "#BDBDBD");
       }
 
       else {
@@ -71,6 +79,7 @@ $(document).ready(function(){
         $(this).css("background-color", "#f43535");
       }
 
+      $(td).effect("highlight", {}, 500);
       $(td).toggleClass("downvoted");
     });
 
@@ -84,18 +93,51 @@ $(document).ready(function(){
       var cell3 = row.insertCell(2);
       var cell4 = row.insertCell(3);
       var cell5 = row.insertCell(4);
+      var cell6 = row.insertCell(5);
       var d = new Date();
 
-      cell1.innerHTML = '<button class="glyphicon glyphicon-minus" aria-hidden="true"></button><span id="count"> 0 </span><button class="glyphicon glyphicon-plus" aria-hidden="true"></button>';
+      cell1.innerHTML = '<button class="glyphicon glyphicon-minus btn-xs" aria-hidden="true"></button><span id="count"> 0 </span><button class="glyphicon glyphicon-plus btn-xs" aria-hidden="true"></button>';
+      $(cell1).toggleClass("vote");
       cell2.innerHTML = document.getElementById('titleBox').value;
       cell3.innerHTML = 'You';
       cell4.innerHTML = d.toDateString();
       cell5.innerHTML = '<div class=scrollable>' + document.getElementById('descriptionBox').value + '</div>';
+      cell6.innerHTML = '<button class="glyphicon glyphicon-pencil btn-sm" aria-hidden="true"></button>' +
+      '<button class="glyphicon glyphicon-trash btn-sm" aria-hidden="true"></button>' +
+      '<button class="btn btn-primary" OnClick=" location.href=\"./tab.html?event=donuts\"">Create</button>';
 
       document.getElementById('titleBox').value = '';
       document.getElementById('descriptionBox').value = '';
 
       $(row).effect("highlight", {}, 3000);
+    });
+
+
+    // Delete an idea from the table
+    $(".glyphicon-trash").click(function() {
+      var row = $(this).parent().parent();
+      $('#deleteIdeaModal').modal('toggle');    // launch confimation modal
+
+      $("#deleteIdea").click(function() {
+        row.css("background-color","#FF3700");
+        row.fadeOut(400, function(){
+            row.remove();
+        });
+      });
+    });
+
+
+    // Edit an idea on the table
+    // BUG TODO: multiple highlights
+    $(".glyphicon-pencil").click(function() {
+      var row = $(this).parent().parent();
+      $('#editIdeaModal').modal('toggle');    // launch modal
+
+      $("#editIdea").click(function() {
+        $(row).effect("highlight", {}, 3000);
+        
+
+      });
     });
 
 
